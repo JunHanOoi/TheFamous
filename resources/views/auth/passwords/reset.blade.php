a22f6dd7b4f08ec62819d2111e75b2fa044b8bcb
chunguantan <chunguantan@1utar.my>
2023-03-12 20:30:51 +0800

    chunguan

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row  center-screen">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
